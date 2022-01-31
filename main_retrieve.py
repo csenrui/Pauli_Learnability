0c28b7e3af8b530912d9895fac43c4b4d231fd49
Senrui Chen <csenrui@gmail.com>
2022-01-30 23:16:00 -0600

    Update main_retrieve.py

--- main_retrieve.py
+++ main_retrieve.py
@@ -7,11 +7,14 @@ from qiskit.providers.aer.noise import NoiseModel
 from qiskit.providers.ibmq.managed import IBMQJobManager
 
 IBMQ.load_account()
-provider = IBMQ.get_provider(hub='ibm-q-research', group='berkeley-6', project='main')
-backend = provider.get_backend('ibmq_manila')
-# provider = IBMQ.get_provider(hub='ibm-q-internal', group='deployed', project='default')
+# provider = IBMQ.get_provider(hub='ibm-q-research', group='berkeley-6', project='main')
+# provider = IBMQ.get_provider(hub='ibm-q-ornl', group='ornl', project='sys-reserve')
+provider = IBMQ.get_provider(hub='ibm-q-ornl', group='ornl', project='phy164')
+# backend = provider.get_backend('ibmq_manila')
+backend = provider.get_backend('ibm_perth')
 
-filename = 'data/' + 'ibmq_experiment_all_20220127_0428006476'
+
+filename = 'data/' + 'ibmq_experiment_all_20220127_1696837399'
 with open(filename, 'rb') as outfile:
     data = pickle.load(outfile)
 token = data["token"]
@@ -25,6 +28,22 @@ for tag in cb:
     results = job_set.results().combine_results()
     print(results)
     data['cb'][tag]["result"] = [results]
+
+int_cb = data['int_cb']
+for tag in int_cb:
+    print(tag)
+    job_set = job_manager.retrieve_job_set(job_set_id=int_cb[tag]['job_set_id'], provider=provider,refresh=True)
+    results = job_set.results().combine_results()
+    print(results)
+    data['int_cb'][tag]["result"] = [results]
+
+intc_cb = data['intc_cb']
+for tag in intc_cb:
+    print(tag)
+    job_set = job_manager.retrieve_job_set(job_set_id=intc_cb[tag]['job_set_id'], provider=provider,refresh=True)
+    results = job_set.results().combine_results()
+    print(results)
+    data['intc_cb'][tag]["result"] = [results]
 
 with open(filename + '_full', 'wb') as outfile:
     pickle.dump(data, outfile)
