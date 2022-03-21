7f6aa24f8e8ddd03f003f00169a01efe7b09c531
Yunchao Liu <yunchaoliu@Yunchaos-MacBook-Pro.local>
2022-03-21 16:01:38 -0700

    clean code

--- clifford.py
+++ clifford.py
@@ -27,11 +27,9 @@ print(C.to_circuit())
 
 paulis = list(itertools.product(['I','X','Y','Z'], repeat=n))
 paulis = [''.join(p) for p in paulis]
-# pauli_list = PauliList(paulis)
 
 evolve_dict = {}
 pattern_dict = {}
-# print(pauli_list)
 
 # invariant = []
 learnable = []
@@ -69,60 +67,6 @@ for p_in in paulis:
 # print(pattern_dict)
 # sys.exit(0)
 
-# unlearnable_copy = copy.deepcopy(unlearnable)
-# learnable_copy = copy.deepcopy(learnable)
-# unlearnable_freedom = 0
-# unlearnable_group = []
-
-# while True:
-#     if len(unlearnable_copy) == 0:
-#         break
-#     # p_label = random.choice(unlearnable_copy)
-#     p_label = unlearnable_copy[0]
-#     group = []
-#     group.append(p_label)
-#     # print(p_label,1)
-#     unlearnable_freedom += 1
-#     learnable_copy.append(p_label)
-#     unlearnable_copy.remove(p_label)
-#     while True:
-#         keep = False
-#         for q_label in copy.deepcopy(unlearnable_copy):
-#             # print(q_label,3)
-#             p_in_1 = Pauli(q_label)
-#             p_out_1 = p_in_1.evolve(C)
-#             for p_in_2_label in learnable_copy:
-#                 p_in_2 = Pauli(p_in_2_label)
-#                 if pattern(n,p_in_2) == pattern(n,p_out_1):
-#                     p_out_2 = p_in_2.evolve(C)
-#                     if pattern(n,p_out_2) == pattern(n,p_in_1):
-#                         # print(q_label,2)
-#                         learnable_copy.append(q_label)
-#                         # print(unlearnable_copy)
-#                         if q_label in unlearnable_copy:
-#                             unlearnable_copy.remove(q_label)
-#                             group.append(q_label)
-#                         keep = True
-#         if keep == False:
-#             unlearnable_group.append(group)
-#             break
-    
-    
-
-
-# # # print("invariant points:", invariant)
-# # print("weight transfer:", weight_transfer, len(weight_transfer))
-# # for t in weight_transfer:
-# #     p,q = t 
-# #     if (q,p) not in weight_transfer:
-# #         print("asymmetric!")
-# print("learnable Pauli fidelities:", learnable)
-# print("unlearnable Pauli fidelities:", unlearnable)
-# print("unlearnable degrees of freedom:", unlearnable_freedom)
-# print("unlearnable groups:", unlearnable_group)
-
-# # print(unlearnable_freedom == int(len(weight_transfer)/2))
-
 unlearnable_copy = copy.deepcopy(unlearnable)
 learnable_copy = []
 unlearnable_freedom = 0
@@ -131,8 +75,8 @@ unlearnable_group = []
 while True:
     if len(unlearnable_copy) == 0:
         break
-    # p_label = random.choice(unlearnable_copy)
-    p_label = unlearnable_copy[0]
+    p_label = random.choice(unlearnable_copy)
+    # p_label = unlearnable_copy[0]
     group = []
     group.append(p_label)
     # print(p_label,1)
